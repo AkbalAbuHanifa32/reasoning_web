fd83780a6c7130eeaacac19b2618f7d1d9be9675
AkbalAbuHanifa32 <103970377+AkbalAbuHanifa32@users.noreply.github.com>
2022-12-27 13:42:10 +0700

    upload file reasoning web
    upload file

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const txtElement = ['high levels of vitamin C', 'vitamin A', 'carbohydrates', 'fiber', 'protein', 'calcium', 'iron', 'magnesium', 'phosphorus', 'potassium', 'folate and choline'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik() {
+    if (count == txtElement.length) {
+        count = 0;
+    }
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ketik').textContent = words;
+
+    if (words.length == currentTxt.length) {
+        count++;
+        txtIndex = 0;
+    }
+    setTimeout(ngetik, 200);
+})();
